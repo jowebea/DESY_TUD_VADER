4919fa13ff4e9df102ed8dcf095f5e80cbee3ee6
Jonas Weiss <jonas@eduroam-0986.desy.de>
2025-09-04 15:23:21 +0200

    Simplefie Test protocol

--- test_vader.py
+++ test_vader.py
@@ -10,32 +10,23 @@ MINI2_PORT = "/dev/ttyACM2"
 MAXI_PORT  = "/dev/ttyACM1"
 
 logging.basicConfig(level=logging.DEBUG)
-print("init Driver...")
+print("##########################")
+print("#### init Driver... ######")
+print("##########################")
 driver = VaderDeviceDriver(MINI1_PORT, MINI2_PORT, MAXI_PORT)
-time.sleep(20)
+time.sleep(1)
 try:
     driver.get_all_status()
+    print("##########################")
     print("Setting N2 to 5nl/min...")
+    print("##########################")
     driver.use_gas(n2=5, co2=0, butan=0)
     time.sleep(0.2)
-    driver.use_gas(n2=5, co2=0, butan=0)
-    time.sleep(0.2)
-    driver.use_gas(n2=5, co2=0, butan=0)
-    time.sleep(0.2)
-
+     
+    print("##########################")
     print("Closing V4 (to let it cool)...")
+    print("##########################")
     driver.maxi.activate_v4(False)
-    time.sleep(0.2)
-
-    print("Status:", driver.get_all_status(mini1_last_n=2))
-    time.sleep(0.2)
-
-    print("Setting Pressure setpoint to 50kPa...")
-    driver.setpoint_pressure(50)
-    time.sleep(0.2)
-    driver.setpoint_pressure(50)
-    time.sleep(0.2)
-    driver.setpoint_pressure(50)
     time.sleep(0.2)
 
     print("Status:", driver.get_all_status(mini1_last_n=2))
