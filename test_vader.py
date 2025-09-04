c4b20a43696fd78a0cc7d8054cc177f1f83bbf03
Jonas Weiss <jonas@eduroam-0986.desy.de>
2025-09-04 20:01:53 +0200

    Adds Presure Monitor

--- test_vader.py
+++ test_vader.py
@@ -26,6 +26,8 @@ for i in range(100):
     print("applying pressure...")
     driver.mini2._send_raw("2; 3; 500")
     time.sleep(2)
+    print(f'Pressure: {driver.mini1.get_last_n(1)}')
     print("applying Vacuum...")
     driver.mini2._send_raw("2; 3; 0")
     time.sleep(8)
+    print(f'Pressure: {driver.mini1.get_last_n(1)}')
